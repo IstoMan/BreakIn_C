ce90b60281797b78b1a9c7827cd110fd6defe4de
IstoMan <suhasnaraparaju@gmail.com>
2026-01-17 13:30:16 +0530

    done

--- main.c
+++ main.c
@@ -9,8 +9,6 @@ int main(void) {
   Game g = initGame();
   InitWindow(g.window_width, g.window_height, g.window_title);
   SetTargetFPS(60);
-  // uint16_t score = 0;
-  //  Counter removed_tiles;
 
   while (!WindowShouldClose()) {
     float deltaTime = GetFrameTime();
